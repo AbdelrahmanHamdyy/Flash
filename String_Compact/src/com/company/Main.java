08d78b8dd2d0c08e3703ddd3d968d745d768fbc0
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-04-19 11:32:06 +0200

    ..

--- String_Compact/src/com/company/Main.java
+++ String_Compact/src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        /*ArrayList<mtWebCrawler> bots = new ArrayList<>();
+        ArrayList<mtWebCrawler> bots = new ArrayList<>();
         bots.add(new mtWebCrawler("https://abcnews.go.com", 1));
         //bots.add(new mtWebCrawler("https://www.npr.org", 2));
         //bots.add(new mtWebCrawler("https://www.nytimes.com", 3));
@@ -16,13 +16,13 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }*/
-       String name="vcAs";
+        }
+      /* String name="vcAs";
         if(name.matches("[a-zA-Z]+")) {
             System.out.println("Bot ID:");
         }
         else
-            System.out.println("hhhh ID:");
+            System.out.println("hhhh ID:");*/
 
     }
 
--- webCrawler/src/main/java/com/company/CompactString.java
+++ webCrawler/src/main/java/com/company/CompactString.java
@@ -6,52 +6,39 @@ import java.util.ArrayList;
 
 public class CompactString {
     ArrayList<String> String_compact=new ArrayList<String>();
+    int count_string=0;
+
     public String String_Compact(Document doc){
-        int count_string=0;
+        count_string=0;
         String compact="";
         String title=doc.title();
-        String p_text =doc.select("p").text();
-        String[] p_text_split = p_text.split(" ");
-        System.out.println(p_text);
-        for(int i=0;i<p_text_split.length;i++) {
-            if (!p_text_split[i].equals("")) {
-                compact+=p_text_split[i].charAt(0);
-                count_string++;
-                if(count_string==50)
-                    break;
-            }
+        compact+= comp ( "p", doc );
+        if(count_string!=50) {
+            compact+= comp ( "span", doc );
         }
         if(count_string!=50) {
-            String span_text =doc.select("span").text();
-            String[] span_text_split = span_text.split(" ");
-            System.out.println(span_text);
-            for(int i=0;i<span_text_split.length;i++) {
-                if (!span_text_split[i].equals("")) {
-                    compact+=span_text_split[i].charAt(0);
-                    count_string++;
-                    if(count_string==50)
-                        break;
-                }
-            }
-        }
-        if(count_string!=50) {
-            String div_text =doc.select("div").text();
-            String[] div_text_split = div_text.split(" ");
-            System.out.println(div_text);
-            for(int i=0;i<div_text_split.length;i++) {
-                if (!div_text_split[i].equals("")) {
-                    compact+=div_text_split[i].charAt(0);
-                    count_string++;
-                    if(count_string==50)
-                        break;
-                }
-            }
+            compact+= comp ( "div", doc );
         }
         System.out.println(compact);
 //        if(String_compact.contains(compact)) {
 //            System.out.println("SPyyyyyyyyyyyyyyyyyyyyyyyyyy");
 //            return null;
 //        }
+        return compact;
+    }
+    private String comp (String tag,Document doc ) {
+        String compact="";
+        String temp_text =doc.select(tag).text();
+        String[] span_text_split = temp_text.split(" ");
+        System.out.println(temp_text);
+        for(int i=0;i<span_text_split.length;i++) {
+            if (!span_text_split[i].equals("")) {
+                compact+=span_text_split[i].charAt(0);
+                count_string++;
+                if(count_string==50)
+                    break;
+            }
+        }
         return compact;
     }
 }
